0b067d211530ccf845562a78a828311fb7ddef7e
Karthik Mullapudi <karthik.mullapudi11@gmail.com>
2017-12-10 17:09:51 -0600

    Search

--- app/views.py
+++ app/views.py
@@ -1,14 +1,24 @@
+from django.db.models import Q
 from django.shortcuts import render, get_object_or_404
 from .models import *
 
 
 def index(request):
-    return render(request, 'app/index.html', {'tags': Tag.objects.filter(breadth='B')[:8],
-                                              'academic_orgs': Organization.objects.filter(tags__name='Academic')[:9],
-                                              'fun_orgs': Organization.objects.filter(tags__name='Fun')[:9],
-                                              'greek_orgs': Organization.objects.filter(tags__name='Greek Life')[:9],
-                                              'sports_orgs': Organization.objects.filter(tags__name='Sports')[:9]
-                                              })
+    queryset_list = Organization.objects.all()
+    query = request.GET.get("q")
+    if query:
+        queryset_list = queryset_list.filter(Q(name__contains=query) |
+                                             Q(abbr__contains=query) |
+                                             Q(tags__name__contains=query)).distinct()
+
+    context = {'tags': Tag.objects.filter(breadth='B')[:8],
+               'academic_orgs': Organization.objects.filter(tags__name='Academic')[:9],
+               'fun_orgs': Organization.objects.filter(tags__name='Fun')[:9],
+               'greek_orgs': Organization.objects.filter(tags__name='Greek Life')[:9],
+               'sports_orgs': Organization.objects.filter(tags__name='Sports')[:9]
+               }
+
+    return render(request, 'app/index.html', context)
 
 
 def rso(request, tag_id):
